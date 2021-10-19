063f9c4449858d9c2ad5d9c0bb38b2ba68cbbc23
rza35 <riza_tire@hotmail.com>
2021-10-19 14:32:40 +0300

    Turizim Acente Sistemi

---
+++ src/main/java/com/patika/OtelAgency/entities/User.java
@@ -0,0 +1,25 @@
+package com.patika.OtelAgency.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String mail;
+}
